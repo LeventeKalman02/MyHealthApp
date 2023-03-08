e580b186b5ac34153f67297185995896c1ee0111
Ronan-Francis <ronananthonyfrancis@gmail.com>
2023-03-08 19:15:41 +0000

    Add project files.

---
+++ MyHealthApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MyHealthApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MyHealthApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MyHealthApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
